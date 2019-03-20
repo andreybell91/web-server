3b68270c4119d355c6e8c1c4047b932843667354
belyaev-a <andreybell91@gmail.com>
2019-03-20 16:19:52 +0300

    Finally

--- src/main/java/com/stc/andreybell/practice/webserver/AppServer.java
+++ src/main/java/com/stc/andreybell/practice/webserver/AppServer.java
@@ -1,24 +1,19 @@
 package com.stc.andreybell.practice.webserver;
 
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.FluxProcessor;
-import reactor.core.publisher.MonoProcessor;
 import reactor.netty.http.server.HttpServer;
-
-import java.nio.file.Paths;
 
 import static reactor.core.publisher.Flux.just;
 
 @Service
 public class AppServer {
 
-
     public void run() {
         HttpServer.create()
                 .host("localhost")
                 .port(8081)
                 .route(routes -> routes
-                        .file("/file", Paths.get("E:\\videos\\Videoclip3.avi"))
+//                        .file("/file", Paths.get("E:\\videos\\Videoclip3.avi"))
 //                        .directory("/directory", Paths.get("E\\videos\\"))
                         .get("/", (req, res) -> res.sendString(just("hello")))
                         .post("/add", (req, res) -> {
